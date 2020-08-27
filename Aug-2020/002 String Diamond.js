325e969ddb876f36f448b6ff0c0b07b7704c3168
Devendra0110 <devendragaud01@gmail.com>
2020-08-27 17:00:02 +0530

    💎 pattern added for javascript

--- Aug-2020/002 String Diamond.js
+++ Aug-2020/002 String Diamond.js
@@ -1,1 +1,45 @@
 // Your task is to return a string that displays a diamond shape on the screen using asterisk (“*”) characters.
+// const readline = require('readline')
+
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let diamondOrder = 0;
+rl.question("Order of diamond patter:  ", function(answer) {
+  // TODO: Log the answer in a database
+  diamondOrder = Number(answer);
+    printDiamond(diamondOrder)
+  rl.close();
+});
+
+const printDiamond = (order) => {
+for (let row = 0; row < order; row++) {
+    for (let blank = order; blank >= row ; blank--) {
+        process.stdout.write(" ");
+    }
+    for (let column = 0; column <= row; column++) {
+        process.stdout.write("*");
+    }
+    for (let column = 0; column < row; column++) {
+        process.stdout.write("*");
+    }
+    console.log();
+}
+for (let row = 0; row <= order; row++) {
+    for (let column = 0; column <= row; column++) {
+        process.stdout.write(" ");
+    }
+    for (let blank = order; blank >= row ; blank--) {
+        process.stdout.write("*");
+    }
+    for (let blank = order; blank > row ; blank--) {
+        process.stdout.write("*");
+    }
+    console.log();
+}
+
+}
